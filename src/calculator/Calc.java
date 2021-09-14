71c7420c1f95590a03a0f4dc4cb77a9128d47bf9
mass0910 <shakeo.0910@gmail.com>
2021-09-14 15:48:16 +0900

    fix calculate timing (timer -> JTextField listener)

--- src/calculator/Calc.java
+++ src/calculator/Calc.java
@@ -1,12 +1,12 @@
 package calculator;
 
 import java.awt.Font;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 public class Calc extends JFrame {
 
@@ -19,20 +19,6 @@ public class Calc extends JFrame {
 
 	Form form = new Form();
 
-	Timer t = new Timer();
-	TimerTask tt = new TimerTask() {
-		public void run() {
-			if (nowform.equals(textbox.getText())) {
-				return;
-			}
-			form.init(textbox.getText());
-			form.split();
-			form.splitOut();
-			label.setText(form.calc());
-			nowform = new String(textbox.getText());
-		}
-	};
-
 	Calc() {
 		setTitle("Calculator");
 		setBounds(100, 100, 640, 200);
@@ -42,17 +28,43 @@ public class Calc extends JFrame {
 		setLayout(null);
 		textbox.setBounds(15, 64, 600, 48);
 		textbox.setFont(font2);
-		label.setText("Show calcurated answer on here.");
+		label.setText("Show calculated answer on here.");
 		label.setBounds(10, 10, 500, 28);
 		label.setFont(font);
 		this.add(textbox);
 		this.add(label);
-
-		t.scheduleAtFixedRate(tt, 0, 100);
 	}
+
+    private void setListener() {
+        textbox.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                calculateForm();
+            }
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                calculateForm();
+            }
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                calculateForm();
+            }
+            private void calculateForm() {
+                if (nowform.equals(textbox.getText())) {
+                    return;
+                }
+                form.init(textbox.getText());
+                form.split();
+                form.splitOut();
+                label.setText(form.calc());
+                nowform = new String(textbox.getText());
+            }
+        });
+    }
 
 	public static void main(String[] args) {
 		Calc m = new Calc();
+        m.setListener();
 	}
 
 }
